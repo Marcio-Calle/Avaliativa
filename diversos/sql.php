28275e4f86dc6863460498af9a3de4a022c24972
Marcio <marciocallejunior753@gmail.com>
2022-11-23 09:35:31 -0300

    envio de imagem

--- diversos/sql.php
+++ diversos/sql.php
@@ -2,7 +2,7 @@
 
     $servername = "localhost";
     $username = "root";
-    $password = "1234";
+    $password = "";
     
     // Create connection
     $conn = new mysqli($servername, $username, $password);
--- registrar.php
+++ registrar.php
@@ -2,6 +2,8 @@
 echo "<pre>";
 if($_FILES['img']['error'] === 0){
     echo "sem erros amigo";
+    
+    move_uploaded_file($_FILES['img']['tmp_name'],'img/'.$_FILES['img']['name']);
 }else{echo 'deu ruim';};
 print_r($_POST);
 ?>
