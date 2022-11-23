87241d66d8ce7650c273f9dbeb2e4d7a4a9f3a9f
sthennyum <0000887996@senaimgaluno.com.br>
2022-11-23 11:35:09 -0300

    registro

--- registro.php
+++ registro.php
@@ -4,7 +4,29 @@ $carrinho = false;
 include 'diversos/header.php';
 include 'diversos/navbar.php';
 ?>
-
+<body>
+    <div class="container">
+        <div class="row justify-content-center mt-5">
+            <div class="col-5">
+            <form>
+                <div class="mb-3">
+                    <label class="form-label">Email address</label>
+                    <input type="email" class="form-control" aria-describedby="emailHelp">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div class="mb-3">
+                    <label  class="form-label">email</label>
+                    <input type="password" class="form-control" >
+                </div>
+                <div class="mb-3">
+                    <label class="form-label">telefone</label>
+                </div>
+                <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
 <?php
 include 'diversos/footer.php';
 
