3263edb8e3ccd72fe50ae7e57421932384bd5e63
0000887996@senaimgaluno.com.br <0000887996@senaimgaluno.com.br>
2022-11-25 11:42:52 -0300

    OLA

--- registrar.php
+++ registrar.php
@@ -1,22 +1,36 @@
 <?php
 include 'diversos/sql.php';
 
-if($_FILES['img']['error'] === 0){
+$acao = $_GET['acao'];
 
-    $imagem = $_FILES['img']['name'];
-    $image_nome = uniqid();
-    $extensao = strtolower(pathinfo($imagem,PATHINFO_EXTENSION));
-    if($extensao != 'jpg' && $extensao != 'png'){
-        die(header('location: cadastroprod.php?erro=32'));
-    }
-    $imagem = $image_nome.'.'.$extensao;
+if($acao == 'cadastro-prod'){
+    if($_FILES['img']['error'] === 0){
 
-    move_uploaded_file($_FILES['img']['tmp_name'],'img/'.$imagem);
+        $imagem = $_FILES['img']['name'];
+        $image_nome = uniqid();
+        $extensao = strtolower(pathinfo($imagem,PATHINFO_EXTENSION));
+        if($extensao != 'jpg' && $extensao != 'png'){
+            die(header('location: cadastroprod.php?erro=32'));
+        }
+        $imagem = $image_nome.'.'.$extensao;
     
+        move_uploaded_file($_FILES['img']['tmp_name'],'img/'.$imagem);
+        
+    
+        $sql = "Insert into imagens (nome_imagem) values ('$imagem')";
+    
+        $conn->query($sql);
+        header('location: cadastroprod.php');
+    }else{echo 'deu ruim';};
+}
 
-    $sql = "Insert into imagens (nome_imagem) values ('$imagem')";
 
-    $conn->query($sql);
-    header('location: cadastroprod.php');
-}else{echo 'deu ruim';};
+if($acao == 'registro-clientes'){
+    $nome = $_POST['nome'];  
+    if($_POST['nome'] == '' || $_POST['telefone'] == '' || $_POST['email'] == '' || $_POST['senha'] == '' || $_POST['confsenha'] == '')
+    echo 'campos vazios';
+    if($_POST["senha"] != $_POST["confsenha"]){
+        echo 'senhas diferentes';
+    } 
+} 
 ?>
--- registro.php
+++ registro.php
@@ -8,25 +8,49 @@ include 'diversos/navbar.php';
     <div class="container">
         <div class="row justify-content-center mt-5">
             <div class="col-5">
-            <form>
+                <div class="card">
+                    <div class="card-header text-center h3">Cadatro de Clientes</div>
+                    <div class="card-body">
+                    <form method="post" action="registrar.php?acao=registro-clientes">
                 <div class="mb-3">
-                    <label class="form-label">Email address</label>
-                    <input type="email" class="form-control" aria-describedby="emailHelp">
-                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                    <label class="form-label">Nome Completo</label>
+                    <input type="text" name="nome" class="form-control" >
                 </div>
                 <div class="mb-3">
-                    <label  class="form-label">email</label>
-                    <input type="password" class="form-control" >
+                    <label  class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control" >
                 </div>
                 <div class="mb-3">
-                    <label class="form-label">telefone</label>
+                    <label class="form-label">Telefone</label>
+                    <input type="text" name="telefone" id="tel" class="form-control" placeholder="Ex.: (00) 00000-0000">               
                 </div>
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <div calss="mb-3">
+                    <Label class="form-label">Senha</label>
+                    <input type="password" name="senha"class="form-control" >
+                </div>
+                <div calss="mb-3">
+                    <Label class="form-label">Confirme Senha</label>
+                    <input type="password" name="confsenha"class="form-control"required>
+                </div>
+                <button type="submit" class="btn btn-primary mt-3">Submit</button>
+                <?php
+                if(isset($_GET['ERRO'])){
+
+                    if($_GET['ERRO']=='telefone'){
+                        echo '<label style="color: red">informações imcorretas</label>';
+                    }
+                } 
+                ?>
                 </form>
+                    </div>
+                </div>
+           
             </div>
         </div>
     </div>
+
 </body>
+
 <?php
 include 'diversos/footer.php';
 
